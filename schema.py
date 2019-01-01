387cf70274aca7b2c945483bf5784a5483226ade
Michael V <michael.verdegaal@hva.nl>
2019-01-01 21:47:16 +0100

    Fix another import error

--- schema.py
+++ schema.py
@@ -2,7 +2,7 @@ import graphene
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyObjectType
 
-from .models.models import UserModel
+from models.models import UserModel
 
 
 class User(SQLAlchemyObjectType):
