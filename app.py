677ecde06afa34dc575d5ffc4cc9c460acb6f521
Michael V <michael.verdegaal@hva.nl>
2019-01-02 14:34:18 +0100

    Add an endpoint for root

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ from schema import schema
 app = Flask(__name__)
 app.debug = True
 
+
+@app.route('/')
+def home():
+    return "Welcome, this is a GraphQL API running on Flask!"
+
+
 app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql',
                                                            schema=schema,
                                                            graphiql=True))
